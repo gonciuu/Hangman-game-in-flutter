c97b6dbde4e1084fb3b5f9a8962ea895ca716b76
andryue <kacperwojak17@gmail.com>
2020-10-28 08:28:03 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Hangman());
+}
+
+class Hangman extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+      ),
+
+    );
+  }
+}
+
