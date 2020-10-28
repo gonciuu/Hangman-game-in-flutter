694dbe605bce0e50fa00725850fde4f536db5a1b
andryue <kacperwojak17@gmail.com>
2020-10-28 09:10:47 +0100

    create welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hangman/screens/main_menu_screen.dart';
 
 void main() {
   runApp(Hangman());
 }
 
 class Hangman extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-      ),
-
+          primaryColor: Colors.greenAccent,
+          accentColor: Colors.grey[900],
+          fontFamily: "ArchitectsDaughter",
+          textTheme: ThemeData.light().textTheme.copyWith(
+              headline1: TextStyle(fontSize: 70.0),
+              headline6: TextStyle(fontSize: 18.0, color: Colors.white))),
+      home: MainMenu(),
     );
   }
 }
-
---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+        backgroundColor: theme.accentColor,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "HANG\nMAN\n_____",
+              textAlign: TextAlign.center,
+              style: theme
+                  .textTheme
+                  .headline1
+                  .copyWith(color: theme.primaryColor),
+            ),SizedBox(height: 80,),
+            Container(
+              width: double.infinity,
+              height: 200,
+              color: theme.primaryColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: FlatButton(
+                            splashColor: theme.primaryColor.withOpacity(0.3),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                            onPressed: () {  },
+                            child: Text("1 Player",style: theme
+                                .textTheme
+                                .headline6),
+                            color: theme.accentColor,
+                            padding:const EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: FlatButton(
+                            splashColor: theme.primaryColor.withOpacity(0.3),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                            onPressed: () {  },
+                            child: Text("2 Players",style: theme
+                                .textTheme
+                                .headline6),
+                            color: theme.accentColor,
+                            padding:const EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                          ),
+                        ),
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  ), Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: FlatButton(
+                            splashColor: theme.primaryColor.withOpacity(0.3),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                            onPressed: () {  },
+                            child: Text("High scores",style: theme
+                                .textTheme
+                                .headline6),
+                            color: theme.accentColor,
+                            padding:const EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: FlatButton(
+                            splashColor: theme.primaryColor.withOpacity(0.3),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                            onPressed: () {  },
+                            child: Text("Info",style: theme
+                                .textTheme
+                                .headline6),
+                            color: theme.accentColor,
+                            padding: EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                          ),
+                        ),
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  )
+                ],
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 20),
+            )
+          ],
+        ));
+  }
+}
