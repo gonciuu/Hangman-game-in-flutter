a75219e40e0404c6444d76e6132bf48243792ad4
andryue <kacperwojak17@gmail.com>
2020-10-28 10:21:43 +0100

    create letter click widget

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hangman/widgets/guess_letter.dart';
-import 'package:hangman/widgets/letter_click.dart';
+import '../widgets/guess_letter.dart';
+import '../widgets/letter_click.dart';
 
 class GameScreen extends StatefulWidget {
   static const routeName = "/game";
+
   @override
   _GameScreenState createState() => _GameScreenState();
 }
@@ -20,35 +21,61 @@ class _GameScreenState extends State<GameScreen> {
           children: [
             Container(
               padding: const EdgeInsets.all(20.0),
-              height: deviceHeight*0.2,
+              height: deviceHeight * 0.2,
               width: double.infinity,
               color: theme.primaryColor,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Text("lives : 4",style: theme.textTheme.headline5,),
-                  Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Hangman.svg/1200px-Hangman.svg.png',fit: BoxFit.cover,),
-                  Text("0:30",style: theme.textTheme.headline5,),
+                  Text(
+                    "lives : 4",
+                    style: theme.textTheme.headline5,
+                  ),
+                  Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Hangman.svg/1200px-Hangman.svg.png',
+                    fit: BoxFit.cover,
+                  ),
+                  Text(
+                    "0:30",
+                    style: theme.textTheme.headline5,
+                  ),
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(30,10,10,0),
+              padding: const EdgeInsets.fromLTRB(30, 10, 10, 0),
               child: Align(
                 alignment: Alignment.bottomLeft,
-                child: Text("Movie",style: TextStyle(
-                  fontSize: 20.0,
-                  color: theme.primaryColor,
-                  fontFamily: "ArchitectsDaughter"
-                ),),
+                child: Text(
+                  "Movie",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: theme.primaryColor,
+                      fontFamily: "ArchitectsDaughter"),
+                ),
               ),
             ),
-            Divider(color: theme.primaryColor,thickness: 2.0,),SizedBox(  height: deviceHeight*0.1,),
+            Divider(
+              color: theme.primaryColor,
+              thickness: 2.0,
+            ),
+            SizedBox(
+              height: deviceHeight * 0.1,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for(int i=0;i<=6;i++) GuessLetter(),
+                for (int i = 0; i <= 6; i++) GuessLetter(),
+              ],
+            ),
+            SizedBox(
+              height: deviceHeight * 0.1,
+            ),
+            for (int i = 0; i <= 3; i++) Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (int j = 0; j <= 5; j++) LetterClick(),
               ],
             )
           ],
--- lib/widgets/letter_click.dart
+++ lib/widgets/letter_click.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LetterClick extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container();
+    final deviceHeight = MediaQuery.of(context).size.height;
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 3,vertical: 3.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.deepPurple[500],
+      ),
+      width: deviceHeight*0.06,
+      height: deviceHeight*0.06,
+      alignment: Alignment.center,
+      child: Text(
+      "A",style: theme.textTheme.headline6.copyWith(fontSize: 30.0),
+      ),
+    );
   }
 }
