05c0f8e7ca6c7ab0a4e658e0df563267aaa98e43
andryue <kacperwojak17@gmail.com>
2020-10-31 14:32:10 +0100

    complete win screen

--- lib/screens/win_screen.dart
+++ lib/screens/win_screen.dart
@@ -2,39 +2,57 @@ import 'package:flutter/material.dart';
 
 class WinScreen extends StatelessWidget {
   static const routeName = "/win-screen";
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("You win",style: theme.textTheme.headline1.copyWith(color: theme.primaryColor),),
-        Text("Your score: 34",style: theme.textTheme.headline1.copyWith(color: theme.primaryColor,fontSize: 28),),SizedBox(height: 30,),
+        Text(
+          "You win",
+          style: theme.textTheme.headline1.copyWith(color: theme.primaryColor),
+        ),
+        Text(
+          "Your score: ${ModalRoute.of(context).settings.arguments}",
+          style: theme.textTheme.headline1
+              .copyWith(color: theme.primaryColor, fontSize: 28),
+        ),
+        SizedBox(
+          height: 30,
+        ),
         FlatButton(
           splashColor: theme.primaryColor.withOpacity(0.3),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-          onPressed: () {  },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          onPressed: () {},
           child: FittedBox(
-            child: Text("Play Again",style: theme
-                .textTheme
-                .headline6.copyWith(color: theme.accentColor)),
+            child: Text("Play Again",
+                style: theme.textTheme.headline6
+                    .copyWith(color: theme.accentColor)),
           ),
           color: theme.primaryColor,
-          padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 30),
-        ),SizedBox(height: 20,),
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        ),
+        SizedBox(
+          height: 20,
+        ),
         FlatButton(
           splashColor: theme.primaryColor.withOpacity(0.3),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-          onPressed: () {  },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          onPressed: () {},
           child: FittedBox(
-            child: Text("Main Menu",style: theme
-                .textTheme
-                .headline6.copyWith(color: theme.accentColor)),
+            child: Text("Main Menu",
+                style: theme.textTheme.headline6
+                    .copyWith(color: theme.accentColor)),
           ),
           color: theme.primaryColor,
-          padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 30),
-
-        ),SizedBox(height: 20,),
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        ),
+        SizedBox(
+          height: 20,
+        ),
       ],
     );
   }
